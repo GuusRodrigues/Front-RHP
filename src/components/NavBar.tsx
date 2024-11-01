183536a57a4b2429d54595a974c62bbbdc3beef0
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-10-31 22:31:53 -0300

    Estrutura do projeto e inicio da navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+export default function NavBar (){
+    return (
+        <nav className='flex justify-between items-center'>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+
+    )
+        
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+//import Image from "next/image";
+//import localFont from "next/font/local";
+import NavBar from "@/components/NavBar";
+
+export default function Home() {
+  return (
+    <main>
+      <NavBar/>
+
+
+    </main>
+  );
+}

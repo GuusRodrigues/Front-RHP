6cb89ecadfc4816abc6c15e300a5db40efa51c18
SenaaMatheus <matheussena2906@gmail.com>
2024-11-03 17:55:01 -0300

    Adição da tela de Login

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,65 @@
+import { useState, ChangeEvent, FormEvent } from 'react';
+import { FaUser, FaLock } from "react-icons/fa";
+
+const LoginForm: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    // Adicione sua lógica de envio do formulário aqui
+  };
+
+  const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <img className="LogoForm" src="/LogoRHP.png" alt="Logo RHP" width={300} height={200} />
+        <div className="input-group">
+          <p>LOGIN</p>
+          <div className="input-with-icon">
+          <FaUser className="icon" />
+            <input
+              type="email"
+              placeholder="Digite seu login"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </div>
+        </div>
+
+        <div className="input-group">
+          <p>SENHA</p>
+          <div className="input-with-icon">
+          <FaLock className="icon" />
+            <input
+              type="password"
+              placeholder="Digite sua senha"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+        </div>
+
+        <div className="remember-me">
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="#">Esqueci a senha</a>
+        </div>
+
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,11 @@
-export default function NavBar (){
-    return (
-        <nav className='flex justify-between items-center'>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
+import Image from 'next/image';
 
-    )
-        
+export default function NavBar() {
+  return (
+    <nav className="flex justify-center items-center h-20">
+      <header className="header">
+        <Image id="LogoName"src="/LogoName.png" alt="Logo RHP" width={200} height={100} />
+      </header>
+    </nav>
+  );
 }

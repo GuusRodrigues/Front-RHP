7c75ce7d6c19d7a8b7e1c3fe548a90a96371f5dc
Bruno <bruninhoalex20@gmail.com>
2024-11-08 20:29:56 -0300

    Alteração no componente NavBar: atualização do logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,8 +4,15 @@ export default function NavBar() {
   return (
     <nav className="flex justify-center items-center h-20">
       <header className="header">
-        <Image id="LogoName"src="/LogoName.png" alt="Logo RHP" width={200} height={100} />
+        <Image 
+          id="RHP_Logo_Horiz"
+          src="/RHP_Logo_Horiz.png" 
+          alt="RHP Logo" 
+          width={100} 
+          height={100} 
+        />
       </header>
     </nav>
   );
 }
+
